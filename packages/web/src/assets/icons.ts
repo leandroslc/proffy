ca7c2d0037b55400ddb8555cc60512aa464a3a21
Leandro Silva Luz do Carmo <leandro_scarmo@hotmail.com>
2020-08-06 20:40:26 -0300

    improvement(web): Added assets catalog for images and icons

---
+++ packages/web/src/assets/icons.ts
@@ -0,0 +1,21 @@
+import back from './icons/back.svg';
+import giveClasses from './icons/give-classes.svg';
+import purpleHeart from './icons/purple-heart.svg';
+import rocket from './icons/rocket.svg';
+import smile from './icons/smile.svg';
+import study from './icons/study.svg';
+import successCheck from './icons/success-check-icon.svg';
+import warning from './icons/warning.svg';
+import whatsapp from './icons/whatsapp.svg';
+
+export const icons = {
+  back,
+  giveClasses,
+  purpleHeart,
+  rocket,
+  smile,
+  study,
+  successCheck,
+  warning,
+  whatsapp,
+};
---
+++ packages/web/src/assets/index.ts
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import hero from './landing.svg';
+import successBackground from './success-background.svg';
+
+export * from './icons';
+
+export const images = {
+  logo,
+  hero,
+  successBackground,
+};
