2aa36a216a6ba27d90cfe0aff627d9e3c4e94434
Leandro Silva Luz do Carmo <leandro_scarmo@hotmail.com>
2020-08-09 19:13:11 -0300

    feat(mobile): Added tab navigation support

---
+++ packages/mobile/src/routes/StudyTabs.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import ClassList from '../pages/ClassList';
+import Favorites from '../pages/Favorites';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const StudyTabs = () => {
+  return (
+    <Navigator
+      tabBarOptions={{
+        style: {
+          elevation: 0,
+          showOpacity: 0,
+          height: 64,
+        },
+        tabStyle: {
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        iconStyle: {
+          flex: 0,
+          width: 20,
+          height: 20,
+        },
+        labelStyle: {
+          fontFamily: 'Archivo_700Bold',
+          fontSize: 13,
+          marginLeft: 16,
+        },
+        inactiveBackgroundColor: '#fafafc',
+        activeBackgroundColor: '#ebebf5',
+        inactiveTintColor: '#c1bccc',
+        activeTintColor: '#32264d',
+      }}
+    >
+      <Screen
+        name="ClassList"
+        component={ClassList}
+        options={{
+          tabBarLabel: 'Proffys',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-easel" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          tabBarLabel: 'Favoritos',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-heart" size={size} color={color} />
+          ),
+        }}
+      />
+    </Navigator>
+  );
+};
+
+export default StudyTabs;
