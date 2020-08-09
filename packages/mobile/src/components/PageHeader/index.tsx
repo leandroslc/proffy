41d0228b7b6e23276112dc4b00bd2371a7cce257
Leandro Silva Luz do Carmo <leandro_scarmo@hotmail.com>
2020-08-09 20:08:56 -0300

    feat(mobile): Created PageHeader component

---
+++ packages/mobile/src/components/PageHeader/index.tsx
@@ -0,0 +1,37 @@
+import React, { PropsWithChildren } from 'react';
+import { Image, Text, View } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import styles from './styles';
+
+import backIcon from '../../assets/images/icons/back.png';
+import logoImg from '../../assets/images/logo.png';
+
+type PageHeaderProps = PropsWithChildren<{
+  title: string;
+}>;
+
+export const PageHeader = (props: PageHeaderProps) => {
+  const { title, children } = props;
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.navigate('Landing');
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <BorderlessButton onPress={handleGoBack}>
+          <Image source={backIcon} resizeMode="contain" />
+        </BorderlessButton>
+
+        <Image source={logoImg} resizeMode="contain" />
+      </View>
+
+      <Text style={styles.title}>{title}</Text>
+
+      {children}
+    </View>
+  );
+};
---
+++ packages/mobile/src/components/PageHeader/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    padding: 40,
+    backgroundColor: '#8257e5',
+  },
+
+  topBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+
+  title: {
+    maxWidth: 160,
+    fontFamily: 'Archivo_700Bold',
+    fontSize: 24,
+    lineHeight: 32,
+    color: '#fff',
+    marginVertical: 40,
+  },
+});
