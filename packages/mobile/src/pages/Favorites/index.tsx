ffb5c65aba2f90219b3647abc8cdb5415bdaf282
Leandro Silva Luz do Carmo <leandro_scarmo@hotmail.com>
2020-08-09 23:43:12 -0300

    feat(mobile): List favorite classes

--- packages/mobile/src/pages/Favorites/index.tsx
+++ packages/mobile/src/pages/Favorites/index.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { ScrollView, View } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import { PageHeader, ClassItem } from '../../components';
+import { ClassInfo } from '../../models';
+import favoritesService from '../../services/favorites';
 import styles from './styles';
 
 const Favorites = () => {
-  return <View style={styles.container} />;
+  const [favorites, setFavorites] = useState([] as ClassInfo[]);
+
+  async function loadFavorites() {
+    const favoriteClasses = await favoritesService.getAll();
+
+    setFavorites(favoriteClasses);
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      loadFavorites();
+    }, []),
+  );
+
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Meus proffys favoritos" />
+
+      <ScrollView
+        style={styles.classList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        {favorites.map((favoriteClass) => (
+          <ClassItem
+            key={favoriteClass.id}
+            classInfo={favoriteClass}
+            favorited={true}
+          />
+        ))}
+      </ScrollView>
+    </View>
+  );
 };
 
 export default Favorites;
--- packages/mobile/src/pages/Favorites/styles.ts
+++ packages/mobile/src/pages/Favorites/styles.ts
@@ -5,4 +5,8 @@ export default StyleSheet.create({
     flex: 1,
     backgroundColor: '#f0f0f7',
   },
+
+  classList: {
+    marginTop: -40,
+  },
 });
